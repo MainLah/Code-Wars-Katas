0df2588c66df858a85ab59413232fdba4f17d169
MainLah <rendrakusumab@gmail.com>
2022-04-18 10:40:44 +0700

    refactored isOddHeavy

--- isOddHeavy.js
+++ isOddHeavy.js
@@ -1,10 +1,10 @@
 function isOddHeavy(n) {
-  let odd = [];
-  let even = [];
+  const odd = [];
+  const even = [];
   for (let i = 0; i < n.length; i++) {
     if (n[i] % 2 !== 0) odd.push(n[i]);
     else even.push(n[i]);
   }
   if (odd.length === 0) return false;
-  return odd.every((e) => even.every((x) => e > x));
+  return even.every((e) => Math.min(...odd));
 }
