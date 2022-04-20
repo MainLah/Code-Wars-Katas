4031410c656d8358c1ef038d6c07d733274c802e
MainLah <rendrakusumab@gmail.com>
2022-04-20 19:37:27 +0700

    finished isSumOfCubes

--- isSumOfCubes.js
+++ isSumOfCubes.js
@@ -1,38 +1,38 @@
-//unfinished
-
-function isSumOfCubes(s) {
-  let result = "";
-  const array = s.split(/\D/).filter((e) => e !== "");
-  const newArray = [].concat(
-    ...array.map((e) => {
-      if (e.length > 3) {
-        return splitIntoThree(e);
-      } else {
-        return e;
-      }
-    })
-  );
-  newArray.forEach((e) =>
-    reducing(e) === Number(e) ? (result += ` ${e}`) : result
-  );
-  return result ? hasilAkhir(result.trim()) : "Unlucky";
+function isSumOfCubes(s){
+  let result = ''
+  const array = s.split(/\D/).filter(e => e !== '')
+  const newArray = [].concat(...array.map(e => {
+    if (e.length > 3) {
+      return splitIntoThree(e)
+    } else {
+      return e
+    }
+  }))
+  newArray.forEach(e => reducing(e) === Number(e) ? result += ` ${e}` : result)
+  return result ? hasilAkhir(result.trim()) : "Unlucky"
 }
 
 function splitIntoThree(string) {
-  const result = [];
+  const result = []
   for (let i = 0; i < string.length; i += 3) {
-    result.push(string.substring(i, i + 3));
+    const current = string.substring(i, i + 3)
+    if (current === current.charAt(0).repeat(2) || current === current.charAt(0).repeat(3)) result.push(current.charAt(0))
+    else result.push(current)
   }
-  return result;
+  return result
 }
 
 function reducing(string) {
-  const array = string.split("").map((e) => Number(e));
-  if (array.length === 1 && array[0] !== 0) return Math.pow(array[0], 3);
-  return array.reduce((a, b) => Math.pow(a, 3) + Math.pow(b, 3));
+  let result = 0
+  const array = string.split("").map(e => Number(e))
+  if (array.length === 1 && array[0] !== 0) return Math.pow(array[0], 3)
+  for (let i = 0; i < array.length; i++) {
+    result += Math.pow(array[i], 3)
+  }
+  return result
 }
-
+  
 function hasilAkhir(string) {
-  const array = string.split("");
-  return `${string} ${array.reduce((a, b) => a + b)} Lucky`;
+  const array = string.split(" ")
+  return `${string} ${array.reduce((a, b) => Number(a) + Number(b))} Lucky`
 }
