630a10f9a5701d7cd228304c80ab56157265940e
MainLah <rendrakusumab@gmail.com>
2022-07-11 09:54:14 +0700

    +1 solution where my anagrams at

--- whereAnagrams.js
+++ whereAnagrams.js
@@ -1,10 +1,16 @@
+// function anagrams(word, words) {
+//   const result = [];
+
+//   words.forEach((e) => {
+//     if (e.split("").sort().join("") === word.split("").sort().join(""))
+//       result.push(e);
+//   });
+
+//   return result;
+// }
+
 function anagrams(word, words) {
-  const result = [];
-
-  words.forEach((e) => {
-    if (e.split("").sort().join("") === word.split("").sort().join(""))
-      result.push(e);
-  });
-
-  return result;
+  return words.filter(
+    (e) => e.split("").sort().join("") === word.split("").sort().join("")
+  );
 }
