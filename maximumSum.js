f54d36cb60e8b3cb6e6650b1e3c4c57eabab7502
Rendra Kusuma <82817358+MainLah@users.noreply.github.com>
2022-07-17 00:13:04 +0700

    Added comments

--- maximumSum.js
+++ maximumSum.js
@@ -1,6 +1,11 @@
 var maxSequence = function (arr) {
   if (arr.length === 0) return 0;
   if (arr.every((e) => e < 0)) return 0;
+  
+  /*
+  * Iterates through every number in array, and then iterate the number again to the array, getting its sum with every other number in the array
+  * Takes the highest possible sum and returns it
+  */
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     let temp = 0;
@@ -9,5 +14,6 @@ var maxSequence = function (arr) {
       if (temp > sum) sum = temp;
     }
   }
+  
   return sum;
 };
